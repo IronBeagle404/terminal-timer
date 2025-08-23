96b8a669c2325d781431ae329399b9e1285b822a
IronBeagle404 <190517473+IronBeagle404@users.noreply.github.com>
2025-08-23 12:13:49 +0200

    docs: add comments to main func

--- main.go
+++ main.go
@@ -14,18 +14,18 @@ import (
 
 func main() {
 
+	// Security / help
 	if len(os.Args) != 2 {
 		fmt.Println("Error : Not enough arguments. Run program with the help flag to display some guidance.")
 		return
 	}
-
 	if os.Args[1] == "help" || strings.Count(os.Args[1], ":") != 2 {
 		fmt.Println("To setup a timer, you can simply run the program followed by the duration, like this : go run . 1:30:0 (this will setup a timer of 1 hour and 30 minutes)\nPlease format timer duration as [hours:minutes:seconds]\nYou can format the numbers in single or double digits, as you prefer")
 		return
 	}
 
+	// Split input time in 3 and check for errors in format/value
 	input := strings.Split(os.Args[1], ":")
-
 	for _, data := range input {
 		dataInt, err := strconv.Atoi(data)
 		if err != nil {
@@ -38,10 +38,10 @@ func main() {
 		}
 	}
 
+	// Assign values to variables and check for errors in value
 	hours, _ := strconv.Atoi(input[0])
 	minutes, _ := strconv.Atoi(input[1])
 	seconds, _ := strconv.Atoi(input[2])
-
 	if minutes > 59 || seconds > 59 {
 		fmt.Println("Error : wrong input format")
 		return
@@ -49,8 +49,8 @@ func main() {
 
 	fmt.Printf("You have set a timer for %d hour(s), %d minute(s), and %d second(s)\n", hours, minutes, seconds)
 
+	// Make a simple countdown from input time in seconds to 0
 	totalTimerValue := seconds + minutes*60 + hours*60*60
-
 	fmt.Printf("%02d:%02d:%02d\n", hours, minutes, seconds)
 	for x := totalTimerValue; x > 0; x-- {
 		seconds--
